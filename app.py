2089ccd685b8c740981d13761b0a4c5a0e46725e
pafernandez-30 <pafernandez@asbarcelona.com>
2026-02-22 08:35:41 +0000

    Tasks done, greet done, nav done

--- app.py
+++ app.py
@@ -12,6 +12,19 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/tasks")
+def tasks():
+    return render_template("tasks.html")
+
+@app.route("/greet/<name>")
+def greet(name):
+    return render_template("greet.html", name=name)
+
+@app.route("/nav")
+def nav():
+    return render_template("nav.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
     
